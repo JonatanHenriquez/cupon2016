5b8eddc16b6f42d912b42c56d61897e051104541
jonatan <mcr.jblh@gmail.com>
2016-03-20 18:25:00 -0600

    Corregido el login y agregado el cajetin de login. Ademas si el usuario esta logueado este cajetin desaparece y se reorganiza la estructura de la pagina

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -44,4 +44,37 @@
             );
         }
 
+
+        public function cajaLoginAction()
+        {
+            $error_ = null;
+            if($this->get("session")->has("_security_default"))
+                return $this->redirectToRoute("homepage");
+
+            $authenticationUtils = $this->get('security.authentication_utils');
+
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+            if($error)
+            {
+                if($error->getMessageKey() == "Account is disabled." )
+                    $error_ = "Usuario deshabilitado.";
+                if($error->getMessageKey() == "Account is locked." )
+                    $error_ = "Usuario bloqueado.";
+                if($error->getMessageKey() == "Invalid credentials." )
+                    $error_ = "Credenciales incorrectas.";
+            }
+
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+            return $this->render(
+                'AppBundle:security:cajaLogin.html.twig',
+                array(
+                    // last username entered by the user
+                    'last_username' => $lastUsername,
+                    'error' => $error_,
+                )
+            );
+        }
+
     }
